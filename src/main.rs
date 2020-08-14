d4ea77f6ef8c4bf23281f2ff1a4d354e0b58c1fe
Danny Tran <dannyytran@gmail.com>
2020-08-13 20:09:51 -0700

    stable

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 use std::env; // to get arugments passed to the program
+use std::thread;
 
 /*
 * Print the number of partitions and the size of each partition
@@ -111,33 +112,59 @@ fn main() {
 
     // CHANGE CODE START: Don't change any code above this line
 
-    // Change the following code to create 2 threads each of which must use map_data()
+    // Change the following code to create 2 each of which must use map_data()
     // function to process one of the two partition
+    let xs_0 = xs[0].clone();
+    let xs_1 = xs[1].clone();
+    
+    let thread1 = thread::spawn(move || map_data(&xs_0));
+    let _thread1 = thread1.join().unwrap();
+    intermediate_sums.push(_thread1);
 
-    intermediate_sums.push(map_data(&xs[0]));
-    intermediate_sums.push(map_data(&xs[1]));
+    let thread2 = thread::spawn(move || map_data(&xs_1));
+    let _thread2 = thread2.join().unwrap();
+    intermediate_sums.push(_thread2);
 
     // CHANGE CODE END: Don't change any code below this line until the next CHANGE CODE comment
 
     // Print the vector with the intermediate sums
-    // println!("Intermediate sums = {:?}", intermediate_sums);
+    println!("Intermediate sums = {:?}", intermediate_sums);
 
     // REDUCE STEP: Process the intermediate result to produce the final result
     let sum = reduce_data(&intermediate_sums);
-    // println!("Sum = {}", sum);
+    println!("Sum = {}", sum);
 
     // CHANGE CODE: Add code that does the following:
     // 1. Calls partition_data to partition the data into equal partitions
     let xs_main = partition_data(num_partitions, &v);
+
     // 2. Calls print_partition_info to print info on the partitions that have been created
     print_partition_info(&xs_main);
-    // 3. Creates one thread per partition and uses each thread to process one partition
-    
-    // 4. Collects the intermediate sums from all the threads
-    // 5. Prints information about the intermediate sums
-    // 5. Calls reduce_data to process the intermediate sums
-    // 6. Prints the final sum computed by reduce_data
 
+    // 3. Creates one thread per partition and uses each thread to process one partition
+    let mut threads : Vec<thread::JoinHandle<usize>> = Vec::new();
+    
+    for i in 0..num_partitions{ //should be a constant when spinning 
+        let x_clone = xs_main[i].clone();
+        threads.push(thread::spawn(move|| map_data(&x_clone)));
+    }
+
+    // 4. Collects the intermediate sums from all the 
+    let mut intermediate_sums2 : Vec<usize> = Vec::new();
+    
+    for t in threads{ 
+        let _r = t.join().unwrap();
+        intermediate_sums2.push(_r);
+    }
+    
+    // 5. Prints information about the intermediate sums
+    println!("Intermediate sums = {:?}", intermediate_sums2);
+
+    // 5. Calls reduce_data to process the intermediate sums
+   let sum2 = reduce_data(&intermediate_sums2);
+
+   // 6. Prints the final sum computed by reduce_data
+   println!("Sum = {}", sum2);
 }
 
 /*
@@ -157,16 +184,16 @@ fn main() {
 * 
 */
 fn partition_data(num_partitions: usize, v: &Vec<usize>) -> Vec<Vec<usize>>{
-    let partition_size = v.len() / num_partitions;
+    let length = v.len();
+    let partition_size = length / num_partitions;
     let mut xs: Vec<Vec<usize>> = Vec::new();
-    let mut n = v.len() % num_partitions;   //n is remainder
+    let mut n = length % num_partitions;   //n is remainder
     let mut r = 0;  // if there is a remainder then r is 1
-    if (n > 0){
+    if n > 0{
         r = 1;
     }
-    let length = v.len();
     let mut j = 0;  // lower bound
-    for i in 0..num_partitions {
+    for _i in 0..num_partitions {
         let v_slice = &v[j..j+partition_size+r];
         j += partition_size+r; 
         if n > 0{   //decrease remaining by 1
